2dc9f29505b9ba7a4f1a3836dfdf7d6f808258b0
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-04 01:14:14 +0530

    added key iterator

--- src/components/ui/Result.tsx
+++ src/components/ui/Result.tsx
@@ -8,12 +8,15 @@ export default function Result({}: Props) {
   const { messages } = useAi();
   //   console.log(messages);
   return (
-    <div className="max-w-2xl px-5 pb-10 mx-auto flex flex-col-reverse scroll-auto" id="content">
+    <div
+      className="max-w-2xl px-5 pb-10 mx-auto flex flex-col-reverse scroll-auto"
+      id="content"
+    >
       {messages.map((message, index) => {
         if (message.role === "user") return;
         if (index !== messages.length - 1) return;
         return (
-          <div className="" >
+          <div className="" key={Math.random()}>
             <p className="text-gray-700">{message.content}</p>
           </div>
         );
