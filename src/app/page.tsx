a2ef010080adb82ce1a64babb29739669bb4836c
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-03 19:06:51 +0530

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="">
       <Header />
+      <Hero />
     </main>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="fixed w-full max-w-4xl left-1/2 top-5 -translate-x-1/2">
+    <header className="px-5 fixed w-full max-w-4xl left-1/2 top-5 -translate-x-1/2">
       <nav className="flex justify-between mb-5 items-center">
         <span className="text-2xl tracking-wide">Senti𝕏.io</span>
         <CommandCenter />
