1a9e1a62f7783c4d9a7b427aa3867fa45548431f
Bhaumik Kore <bhaumik.kore31@gmail.com>
2023-08-05 00:16:11 +0530

    performance fix

--- src/app/api/tweet/route.ts
+++ src/app/api/tweet/route.ts
@@ -1,18 +1,23 @@
 import { StreamingTextResponse, LangChainStream } from "ai";
 import {} from "openai-edge";
 import { OpenAIChat } from "langchain/llms/openai";
-import { PromptTemplate } from "langchain/prompts";
+import {
+  PromptTemplate,
+  FewShotPromptTemplate,
+  LengthBasedExampleSelector,
+  BaseExampleSelector,
+} from "langchain/prompts";
+
 import { Message } from "ai";
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  //   const { tweet } = await req.json();
   const { messages }: { messages: Message[]; config: string } =
     await req.json();
   const tweet =
     messages.at(-1)?.role === "user" ? messages.at(-1)?.content : "";
-  console.log("tweet", tweet);
+  // console.log("tweet", tweet);
   const { stream, handlers } = LangChainStream();
 
   const llm = new OpenAIChat({
@@ -20,23 +25,40 @@ export async function POST(req: Request) {
     openAIApiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
     modelName: "gpt-3.5-turbo",
   });
-  const oneInputPrompt = new PromptTemplate({
+
+  const examplePrompt = new PromptTemplate({
+    inputVariables: ["input", "output"],
+    template: "Input: {input}\nOutput: {output}",
+  });
+
+  const exampleSelector = await LengthBasedExampleSelector.fromExamples(
+    [
+      {
+        input: "I hate the movie Tenet.",
+        output: `The sentiment in the above tweet is predominantly negative.\nThe subject emotion attached is hate and the subject is the movie Tenet.`,
+      },
+      {
+        input: "I love watching movies, but i absolutely hate horror movies.",
+        output: `The sentiment in the above tweet is positive for watching movies but negative for horror movies.\nThe sentiment analysis reveals that the subject "watching movies" is associated with the emotion of "love," while the subject "horror movies" is associated with the emotion of "hate"`,
+      },
+      {
+        input: "I hate video games, especially the violent ones.",
+        output: `The sentiment in the above tweet is negative for video games.\nThe sentiment analysis indicates that the subject "video games" is associated with the emotion of "hate," and specifically, the subject "violent ones" is also associated with the emotion of "hate."`,
+      },
+    ],
+    {
+      examplePrompt,
+    }
+  );
+  const dynamicPrompt = new FewShotPromptTemplate({
+    exampleSelector,
+    examplePrompt,
+    prefix: "Give the antonym of every input",
+    suffix: "Input: {tweet}\nOutput:",
     inputVariables: ["tweet"],
-    template: `Do sentimental Analysis on the following text and give the subject that is liked or hated : {tweet}
-      Example,
-
-      Input: "I hate the movie Tenet".
-      Output: "The sentiment in the above tweet is predominantly negative.\n The subject emotion attached is hate and the subject is the movie Tenet".
-
-      Input: "I love watching movies, but i absolutely hate horror movies.".
-      Output: "The sentiment in the above tweet is positive for watching movies but negative for horror movies.\n  The sentiment analysis reveals that the subject "watching movies" is associated with the emotion of "love," while the subject "horror movies" is associated with the emotion of "hate"".
-
-      Input: "I hate video games, especially the violent ones.".
-      Output: "The sentiment in the above tweet is negative for video games.\n  the sentiment analysis indicates that the subject "video games" is associated with the emotion of "hate," and specifically, the subject "violent ones" is also associated with the emotion of "hate."`,
   });
-  const formattedPrompt = await oneInputPrompt.format({
-    tweet,
-  });
+
+  const formattedPrompt = await dynamicPrompt.format({ tweet });
 
   llm.call(formattedPrompt, {}, [handlers]).catch(console.error);
   return new StreamingTextResponse(stream);
